f58093a7d68f16536dda8cbac2c160d96ce4e084
Marcelo Melo <marcelosdm@gmail.com>
2019-11-10 17:06:19 -0300

    Class 2 - Challenge

--- src/com/curso/pratica/controller/EnderecoController.java
+++ src/com/curso/pratica/controller/EnderecoController.java
@@ -4,22 +4,21 @@ import com.curso.pratica.model.Endereco;
 
 public class EnderecoController {
 
-	
 	/**
 	 * Método com responsabilidade para criação de endereços
 	 */
 	public void imprimeEndereco() {
 		Endereco endereco = new Endereco(1, "Rua", "Arnulfo de Lima", 2385, "Vila Santa Cruz");
-		
-		System.out.println(String.format("[%s] Endereço: %s %s, %s - %s ",
-				endereco.getId(), endereco.getLogradouro(), endereco.getNome(), endereco.getNumero(), endereco.getBairro()));
+
+		System.out.println(String.format("[%s] Endereço: %s %s, %s - %s ", endereco.getId(), endereco.getLogradouro(),
+				endereco.getNome(), endereco.getNumero(), endereco.getBairro()));
 	}
-	
+
 	/**
 	 * Método para impressão de endereços, conforme endereço passado por parâmetro
 	 */
 	public void imprimeEndereco(Endereco endereco) {
-		System.out.println(String.format("[%s] Endereço: %s %s, %s - %s ",
-				endereco.getId(), endereco.getLogradouro(), endereco.getNome(), endereco.getNumero(), endereco.getBairro()));
+		System.out.println(String.format("[%s] Endereço: %s %s, %s - %s ", endereco.getId(), endereco.getLogradouro(),
+				endereco.getNome(), endereco.getNumero(), endereco.getBairro()));
 	}
 }
